393488b173e322286a8cdbd94e4a76642a8f187f
Matthias <m@tth.es>
2025-08-11 23:44:02 +0200

    update

--- packages/responsive/src/index.ts
+++ packages/responsive/src/index.ts
@@ -1,2 +1,10 @@
+import { TjResponsiveElement } from './components/tj-responsive/tj-responsive';
+
 export * from './components/tj-responsive/tj-responsive';
 export * from './lib/responsive';
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'tj-responsive': TjResponsiveElement;
+  }
+}
