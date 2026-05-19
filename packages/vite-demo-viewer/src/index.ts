5fccc86947a4a9382bd2ea72efc0556d288293b9
Matthias <m@tth.es>
2026-05-19 17:18:16 +0200

    update

--- packages/vite-demo-viewer/src/index.ts
+++ packages/vite-demo-viewer/src/index.ts
@@ -1,15 +1,5 @@
-import type {
-  TControlDefinition,
-  TDemoDefinition,
-  TNavBranchNode,
-  TNavData,
-  TNavLeafNode,
-  TNavTreeNode,
-} from '@trunkjs/demo-viewer';
-
 export * from './lib/tjDemoViewerPlugin';
-export type { TControlDefinition, TDemoDefinition, TNavBranchNode, TNavData, TNavLeafNode, TNavTreeNode };
 
-export function defineDemo(demo: TDemoDefinition): TDemoDefinition {
+export function defineDemo<TDemo>(demo: TDemo): TDemo {
   return demo;
 }
